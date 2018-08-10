00585e7ff809d90fb9aa4b35466ca5e5fc1d4bb3
Łukasz Gretka <lukasz.gretka@gmail.com>
2018-08-10 19:24:16 +0200

    Build http request for api data

---
+++ WowCharComparerLib/BlizzardAPIManager.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WowCharComparerLib
+{
+    public class BlizzardAPIResponse
+    {
+        public string Data { get; set; }
+        public Exception Exception { get; set; } = null;
+    }
+
+    public class BlizzardAPIManager
+    {
+        public static async Task<BlizzardAPIResponse> GetCharacterDataAsJsonAsync(string characterName, BlizzardRealms realm)
+        {
+            BlizzardAPIResponse blizzardAPIResponse = new BlizzardAPIResponse();
+            Uri uriAddress = GenerateAPIRequestLink(characterName, realm);
+
+            try
+            {
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    blizzardAPIResponse.Data = await httpClient.GetStringAsync(uriAddress.AbsoluteUri);
+                }
+            }
+            catch(Exception ex)
+            {
+                blizzardAPIResponse = new BlizzardAPIResponse()
+                {
+                    Data = string.Empty,
+                    Exception = ex
+                };
+            }
+
+            return blizzardAPIResponse;
+        }
+
+        public static BlizzardAPIResponse GetCharacterDataAsJson(string characterName, BlizzardRealms realm)
+        {
+            BlizzardAPIResponse blizzardAPIResponse = new BlizzardAPIResponse();
+            Uri uriAddress = GenerateAPIRequestLink(characterName, realm);
+
+            try
+            {
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    blizzardAPIResponse.Data = httpClient.GetStringAsync(uriAddress.AbsoluteUri).Result;
+                }
+            }
+            catch(Exception ex)
+            {
+                blizzardAPIResponse = new BlizzardAPIResponse()
+                {
+                    Data = string.Empty,
+                    Exception = ex
+                };
+            }
+
+            return blizzardAPIResponse;
+        }
+
+        private static Uri GenerateAPIRequestLink(string characterName, BlizzardRealms realm)
+        {
+            string generatedLink = String.Format("{0}/{1}/{2}?locale=en_GB&{3}", APIConf.BlizzardAPICharacterAddress, realm.ToString(), characterName, APIConf.APIKey);
+            return new Uri(generatedLink);
+        }
+    }
+}
--- WowCharComparerWPF/MainWindow.xaml.cs
+++ WowCharComparerWPF/MainWindow.xaml.cs
@@ -4,14 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using WowCharComparerLib;
 
 namespace WowCharComparerWPF
 {
@@ -23,6 +16,11 @@ namespace WowCharComparerWPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+             JsonResultTextBox.Text  = BlizzardAPIManager.GetDataAsJsonFromUrl();
         }
     }
 }
