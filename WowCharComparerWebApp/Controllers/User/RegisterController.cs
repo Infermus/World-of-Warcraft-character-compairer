3a260a803a5663cdb2762cd98371dbd6e8b7a625
Łukasz Gretka <lukasz.gretka@gmail.com>
2019-06-08 18:36:30 +0200

    Small changes after code review

--- WowCharComparerWebApp/Controllers/User/RegisterController.cs
+++ WowCharComparerWebApp/Controllers/User/RegisterController.cs
@@ -3,9 +3,7 @@ using Microsoft.EntityFrameworkCore.Storage;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using WowCharComparerWebApp.Data.Database;
-using System.Timers;
 
 namespace WowCharComparerWebApp.Controllers.User
 {
@@ -19,64 +17,61 @@ namespace WowCharComparerWebApp.Controllers.User
         [HttpPost]
         public IActionResult PerformUserRegister(string accountName, string userPassword, string userEmail)
         {
-            using (ComparerDatabaseContext db = new ComparerDatabaseContext())
+            Models.Internal.User user = new Models.Internal.User();
+
+            try
             {
+                using (ComparerDatabaseContext db = new ComparerDatabaseContext())
                 using (IDbContextTransaction transaction = db.Database.BeginTransaction())
                 {
                     List<bool> CheckedValidators = new List<bool>()
                     {
-                        CheckUsername(accountName,db),
+                        CheckUsername(accountName, db),
                         CheckPassword(userPassword),
-                        CheckEmail(userEmail,db)
-
+                        CheckEmail(userEmail, db)
                     };
 
                     if (CheckedValidators.All(x => x))
                     {
-                            db.Users.Add(new Models.Internal.User()
-                            {
-                                Email = userEmail,
-                                Password = userPassword,
-                                Nickname = accountName,
-                                ID = new Guid(),
-                                IsOnline = false,
-                                LastLoginDate = DateTime.MinValue,
-                                RegistrationDate = DateTime.Now,
-                                Verified = false
-                            });
+                        user = new Models.Internal.User()
+                        {
+                            Email = userEmail,
+                            Password = userPassword,
+                            Nickname = accountName,
+                            ID = new Guid(),
+                            IsOnline = false,
+                            LastLoginDate = DateTime.MinValue,
+                            RegistrationDate = DateTime.Now,
+                            Verified = false
+                        };
 
+                        db.Users.Add(user);
                         db.SaveChanges();
                         transaction.Commit();
                     }
                 }
             }
+            catch (Exception ex)
+            {
+                // return HTML view which shows the user that he cannot register because of technial problem
+            }
 
-            return View();
+            return View(user);
         }
 
         public bool CheckPassword(string password)
         {
-            bool checkedPassword = password.Any(char.IsDigit) & password.Any(char.IsUpper) & password.Length >= 8
-                                                              ? true
-                                                              : false;
-
-            return checkedPassword;
+            return password.Any(char.IsDigit) & password.Any(char.IsUpper) & password.Length >= 8;
         }
 
         public bool CheckUsername(string accountName, ComparerDatabaseContext db)
         {
-            bool checkedAccountName = accountName.Length >= 6 && db.Users.All(x => x.Nickname != accountName) 
-                                                               ? true
-                                                               : false;
-
-
-            return checkedAccountName;
+            return accountName.Length >= 6 && db.Users.All(x => x.Nickname != accountName);
         }
 
         public bool CheckEmail(string email, ComparerDatabaseContext db)
         {
-            bool checkedEmail = email.Contains("@") && email.Contains(".");
-            return checkedEmail;
+            return email.Contains("@") && email.Contains(".");
         }
     }
 }
