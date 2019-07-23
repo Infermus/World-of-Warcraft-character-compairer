889063ede5712bf304492450bbc29ba4e0e98632
Łukasz G <lukasz.gretka@gmail.com>
2019-07-23 19:07:11 +0200

    * CharacterComparerController cleans up * Bootstrap updated to version 4.3.1

--- WowCharComparerWebApp/Controllers/Character/CharacterComparatorController.cs
+++ WowCharComparerWebApp/Controllers/Character/CharacterComparatorController.cs
@@ -1,18 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using WowCharComparerWebApp.Configuration;
 using WowCharComparerWebApp.Data.ApiRequests;
 using WowCharComparerWebApp.Data.Connection;
 using WowCharComparerWebApp.Data.Database;
-using WowCharComparerWebApp.Data.Database.Repository.Others;
 using WowCharComparerWebApp.Data.Helpers;
 using WowCharComparerWebApp.Enums;
 using WowCharComparerWebApp.Enums.BlizzardAPIFields;
-using WowCharComparerWebApp.Enums.Locale;
 using WowCharComparerWebApp.Logic.DataResources;
 using WowCharComparerWebApp.Models;
-using WowCharComparerWebApp.Models.Achievement;
 using WowCharComparerWebApp.Models.CharacterProfile;
 using WowCharComparerWebApp.Models.Servers;
 
@@ -32,10 +29,10 @@ namespace WowCharComparerWebApp.Controllers.CharacterControllers
             _iAPIDataRequestManager = iAPIDataRequestManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             //Note: Local variable to to avoid 2x requests for both players at default;
-            List<string> defaultRegionForBothPlayer = GetRealmListByRegion(Region.Europe);
+            List<string> defaultRegionForBothPlayer = await new RealmsRequests(_comparerDatabaseContext).GetRealmListByRegion(Region.Europe);
 
             currentRealmListPlayerLeft = defaultRegionForBothPlayer;
             currentRealmListPlayerRight = defaultRegionForBothPlayer;
@@ -46,69 +43,7 @@ namespace WowCharComparerWebApp.Controllers.CharacterControllers
             return View();
         }
 
-        #region Selecting region actions (Experimental)
-
-        [Route("selected-region-left")]
-        public IActionResult SelectedRegionLeftPlayer(Region region)
-        {
-            currentRealmListPlayerLeft = GetRealmListByRegion(region);
-
-            ViewData["realmsListLeftPlayer"] = currentRealmListPlayerLeft;
-            ViewData["realmsListRightPlayer"] = currentRealmListPlayerRight;
-
-            return View("Index");
-        }
-
-        [Route("selected-region-right")]
-        public IActionResult SelectedRegionRightPlayer(Region region)
-        {
-            currentRealmListPlayerRight = GetRealmListByRegion(region);
-
-            ViewData["realmsListLeftPlayer"] = currentRealmListPlayerLeft;
-            ViewData["realmsListRightPlayer"] = currentRealmListPlayerRight;
-
-            return View("Index");
-        }
-
-        #endregion
-
-        public IActionResult TestActionOne()
-        {
-            return StatusCode(404);
-        }
-
-        public IActionResult TestActionTwo()
-        {
-            new DbAccessBonusStats(_comparerDatabaseContext).InsertBonusStatsTableFromJsonFile();
-            return Content("Action two - executed");
-        }
-
-        public IActionResult TestActionThree()
-        {
-            string returnContent = string.Empty;
-
-            var data = new DataResources(_comparerDatabaseContext).GetCharacterAchievements(new RequestLocalization()
-            {
-                CoreRegionUrlAddress = APIConf.BlizzadAPIAddressWrapper[Region.Europe],
-                Realm = new Realm() { Locale = EULocale.en_GB.ToString() }
-            });
-
-            if (data.Result.Exception is null)
-            {
-                Achievement parsedResult = JsonProcessing.DeserializeJsonData<Achievement>(data.Result.Data);
-                new DbAccessAchievements(_comparerDatabaseContext).InsertAllAchievementsDataFromApiRequest(parsedResult);
-
-                returnContent = "Test action three executed - achievements insertion to database";
-            }
-            else
-            {
-                returnContent = data.Result.Exception.Message;
-            }
-
-            return Content(returnContent);
-        }
-
-        public IActionResult ComparePlayers(ExtendedCharacterModel firstPlayer, ExtendedCharacterModel secondPlayer)
+        public async Task<IActionResult> ComparePlayers(ExtendedCharacterModel firstPlayer, ExtendedCharacterModel secondPlayer)
         {
             List<ProcessedCharacterModel> processedCharacterData = new List<ProcessedCharacterModel>();
 
@@ -122,12 +57,12 @@ namespace WowCharComparerWebApp.Controllers.CharacterControllers
             foreach (string characterName in new List<string>() { firstPlayer.Name, secondPlayer.Name })
             {
                 //TODO Get input from view to fill up character fields (check boxes which determines what to compare)
-                var result = new CharacterRequests(_iAPIDataRequestManager).GetCharacterDataAsJsonAsync(characterName, requestLocalization,
-                                                                            new List<CharacterFields>()
-                                                                            {
-                                                                                CharacterFields.Items,
-                                                                                CharacterFields.Achievements
-                                                                            }).Result;
+                var result = await new CharacterRequests(_iAPIDataRequestManager).GetCharacterDataAsJsonAsync(characterName, requestLocalization,
+                                                                                    new List<CharacterFields>()
+                                                                                    {
+                                                                                        CharacterFields.Items,
+                                                                                        CharacterFields.Achievements
+                                                                                    });
 
                 ExtendedCharacterModel currentCharacter = JsonProcessing.DeserializeJsonData<ExtendedCharacterModel>(result.Data);
                 CharacterExtendedDataManager characterDataManager = new CharacterExtendedDataManager(_comparerDatabaseContext);
@@ -156,40 +91,6 @@ namespace WowCharComparerWebApp.Controllers.CharacterControllers
             }
 
             return View("CompareResult");
-        }
-
-        private List<string> GetRealmListByRegion(Region region)
-        {
-            List<string> realmsNames = new List<string>();
-            string regionCoreAdress = string.Empty;
-
-            try
-            {
-                if (APIConf.BlizzadAPIAddressWrapper.TryGetValue(region, out regionCoreAdress) == false)
-                {
-                    throw new KeyNotFoundException($"Cannot find region in {APIConf.BlizzadAPIAddressWrapper.GetType().Name} dictionary");
-                }
-
-                RequestLocalization requestLocalization = new RequestLocalization()
-                {
-                    CoreRegionUrlAddress = regionCoreAdress,
-                };
-
-                RealmsRequests realmsRequests = new RealmsRequests(_comparerDatabaseContext);
-                var realmResponse = realmsRequests.GetRealmsDataAsJsonAsync(requestLocalization);
-                RealmStatus realmStatus = JsonProcessing.DeserializeJsonData<RealmStatus>(realmResponse.Result.Data);
-
-                foreach (Realm realmsData in realmStatus.Realms)
-                {
-                    realmsNames.Add(realmsData.Name);
-                }
-            }
-            catch (Exception)
-            {
-                realmsNames = new List<string>();
-            }
-
-            return realmsNames;
         }
     }
 }
--- WowCharComparerWebApp/Data/ApiRequests/RealmsRequests.cs
+++ WowCharComparerWebApp/Data/ApiRequests/RealmsRequests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WowCharComparerWebApp.Configuration;
 using WowCharComparerWebApp.Data.Connection;
 using WowCharComparerWebApp.Data.Database;
 using WowCharComparerWebApp.Data.Helpers;
@@ -20,13 +21,41 @@ namespace WowCharComparerWebApp.Data.ApiRequests
             _comparerDatabaseContext = comparerDatabaseContext;
         }
 
-        internal async Task<BlizzardAPIResponse> GetRealmsDataAsJsonAsync(RequestLocalization requestLocalization)
+        internal async Task<List<string>> GetRealmListByRegion(Region region)
         {
-            List<KeyValuePair<string, string>> parameters = new List<KeyValuePair<string, string>>();
-            Uri uriAddress = RequestLinkFormater.GenerateAPIRequestLink(BlizzardAPIProfiles.Realm, requestLocalization, parameters, "status"); //TODO replace string
-            
+            List<string> realmsNames = new List<string>();
+            string regionCoreAdress = string.Empty;
 
-            return await new APIDataRequestManager(_comparerDatabaseContext).GetDataByHttpRequest<BlizzardAPIResponse>(uriAddress);
+            try
+            {
+                if (APIConf.BlizzadAPIAddressWrapper.TryGetValue(region, out regionCoreAdress) == false)
+                {
+                    throw new KeyNotFoundException($"Cannot find region in {APIConf.BlizzadAPIAddressWrapper.GetType().Name} dictionary");
+                }
+
+                var requestLocalization = new RequestLocalization()
+                {
+                    CoreRegionUrlAddress = regionCoreAdress,
+                };
+
+                RealmsRequests realmsRequests = new RealmsRequests(_comparerDatabaseContext);
+
+                var parameters = new List<KeyValuePair<string, string>>();
+                Uri uriAddress = RequestLinkFormater.GenerateAPIRequestLink(BlizzardAPIProfiles.Realm, requestLocalization, parameters, "status"); //TODO replace string
+                var realmResponse = await new APIDataRequestManager(_comparerDatabaseContext).GetDataByHttpRequest<BlizzardAPIResponse>(uriAddress);
+                RealmStatus realmStatus = JsonProcessing.DeserializeJsonData<RealmStatus>(realmResponse.Data);
+
+                foreach (Realm realmsData in realmStatus.Realms)
+                {
+                    realmsNames.Add(realmsData.Name);
+                }
+            }
+            catch (Exception)
+            {
+                realmsNames = new List<string>();
+            }
+
+            return realmsNames;
         }
     }
 }
